22bf446ec91e748b7d6655e08b3fb2183d389052
Diego Tonini <C-Diego.Tonini@charter.com>
2018-03-19 10:30:09 -0300

    fixing wrong models

--- src/main/java/br/com/santeri/repository/AuthorRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.santeri.repository;
-
-import br.com.santeri.model.Author;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
-public interface AuthorRepository extends ReactiveMongoRepository<Author, String> {
-	
-}
--- src/main/java/br/com/santeri/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.santeri.repository;
-
-
-import br.com.santeri.model.Book;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
-public interface BookRepository extends ReactiveMongoRepository<Book, String> {
-	
-}
